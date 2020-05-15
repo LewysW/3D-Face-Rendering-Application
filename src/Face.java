0b9b72a21a82f3155ee42e624bb1d2ea6b8fadd7
lewys <lewysward99@gmail.com>
2020-05-15 12:46:24 +0100

    Changed sorting order to reverse sorted by average depth

--- src/Face.java
+++ src/Face.java
@@ -10,35 +10,53 @@ public class Face {
         this.triangles = triangles;
     }
 
+    /**
+     * Generates the new triangles for the synthetic face
+     * using the three reference faces and their weights
+     * @param faces - reference faces
+     * @param weights - influence of each face based on interpolation determined by click
+     */
     Face(ArrayList<Face> faces, ArrayList<Double> weights) {
         int numTriangles = faces.get(0).triangles.size();
 
-        int i = 0;
-
+        //For each triangle in provided faces
         for (int t = 0; t < numTriangles; t++) {
             int numVertices = faces.get(0).triangles.get(0).vertices.size();
             Triangle syntheticTriangle = new Triangle();
 
+            //For each vertex in the current triangle
             for (int v = 0; v < numVertices; v++) {
                 Vertex syntheticVertex = new Vertex(0, 0, 0, 0, 0, 0);
 
+                //Get the current vertex for each face
                 Vertex vertex0 = faces.get(0).triangles.get(t).vertices.get(v);
                 Vertex vertex1 = faces.get(1).triangles.get(t).vertices.get(v);
                 Vertex vertex2 = faces.get(2).triangles.get(t).vertices.get(v);
 
+                //Generate a new 'synthetic' vertex using the three existing vertices and the weigths of each face
                 syntheticVertex = apply(syntheticVertex, vertex0, weights.get(0));
                 syntheticVertex = apply(syntheticVertex, vertex1, weights.get(1));
                 syntheticVertex = apply(syntheticVertex, vertex2, weights.get(2));
 
+                //Add the new 'synthetic' vertex to a new 'synthetic' triangle
                 syntheticTriangle.vertices.add(syntheticVertex);
             }
 
+            //Add the synthetic triangle to the triangles of the new face
             this.triangles.add(syntheticTriangle);
         }
 
-        Collections.sort(triangles, new TriangleComparator());
+        //Reverse sort triangles by average depth for use in painter's algorithm
+        Collections.sort(triangles, new TriangleComparator().reversed());
     }
 
+    /**
+     * Apply influence of vertex v * weight to synthetic vertex
+     * @param synthetic - new synthetic vertex
+     * @param v - vertex of reference face
+     * @param weight - weight to apply to reference face vertex
+     * @return synthetic vertex with applied weight of vertex v
+     */
     Vertex apply(Vertex synthetic, Vertex v, double weight) {
         synthetic.x += weight * v.x;
         synthetic.y += weight * v.y;
